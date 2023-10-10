6d1161005610f58afef249d7d15209e3c2f9d863
Shubham Saxena <shubh.lpu16@gmail.com>
2023-10-10 18:57:10 +0530

    chore: remove unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from '@/routes/home'
 function App() {
   return (
     <Routes>
-      <Route path="/a" element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import reportWebVitals from './reportWebVitals'
-import { theme } from '@/styles/theme'
+
+import App from '@/App'
+import * as serviceWorkerRegistration from '@/serviceWorkerRegistration'
+import reportWebVitals from '@/reportWebVitals'
 import '@/styles/globals.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Router>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </Router>
 )
 
