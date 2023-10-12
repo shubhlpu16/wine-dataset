49d3fb4bf32bb776fa7831db4abdb655bebf04a8
Shubham Saxena <shubh.lpu16@gmail.com>
2023-10-12 12:15:31 +0530

    chore: add coments

--- src/utils/math-functions.ts
+++ src/utils/math-functions.ts
@@ -1,12 +1,15 @@
+// Function to calculate mean
 export const calculateMean = (data: number[]): number => {
   return data.reduce((a, b) => a + b) / data.length
 }
 
+// Function to calculate median
 export const calculateMedian = (data: number[]): number => {
   const sortedData = data.sort((a, b) => a - b)
   return sortedData[Math.floor(sortedData.length / 2)]
 }
 
+//Function to calculate mode
 export const calculateMode = (data: number[]): number => {
   const frequencyArray: { [key: string]: number } = {}
   data.forEach((item) => {
@@ -23,10 +26,12 @@ export const calculateMode = (data: number[]): number => {
   return mode
 }
 
+// Utility To parse array values to float from number or string
 export const convertArrayToFloatArray = (arr: (number | string)[]) => {
   return arr.map((item) => parseFloat(item.toString()))
 }
 
+// Utility to format the decimal upto 3 decimal places and parse them to float
 export const formatToDecimal = (number: number) => {
   return parseFloat(number.toFixed(3))
 }
